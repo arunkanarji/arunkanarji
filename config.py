bd1fb8248b9e41857cc1995902758fd99dbd96bb
arunkanarji <143004206+arunkanarji@users.noreply.github.com>
2024-09-13 22:24:36 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7069808302:AAEAbiw3JONIE5KWX77O9LpS-oFG-9NKW6I")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("BGS_AYUSH")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5633556409")
+
+
+
+
+
+
+
+  
